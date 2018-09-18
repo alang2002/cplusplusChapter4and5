ae71aaf3b8ffb5cbc38f95d9a2480571ca256980
Aaron Lang <blackthornlm@gmail.com>
2018-09-18 10:35:02 -0500

    Right Triangle completed!!

--- Triangle Calculator/Triangle Calculator/Header.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-void solveForA();
-void rightTriangle();
-void solveForB();
--- Triangle Calculator/Triangle Calculator/Source.cpp
+++ Triangle Calculator/Triangle Calculator/Source.cpp
@@ -1,9 +1,78 @@
+// Author: Aaron Lang
+// 9/18/18
 #include <iostream>
 #include <string>
 #include <math.h>
-#include "Header.h"
-#include "RightTriangle.cpp"
 using namespace std;
+double perimeter;
+double area;
+
+void setPerimeter(double sideA, double sideB, double sideC) {
+	perimeter = sideA + sideB + sideC;
+}
+
+void setRightArea(double sideA, double sideB) {
+	area = (sideA * sideB) / 2;
+}
+
+double getPerimeter() {
+	return perimeter;
+}
+
+double getArea() {
+	return area;
+}
+
+double solveForA(double sideB, double sideC) {
+	double newSideB = sideB * sideB;
+	double newSideC = sideC * sideC;
+	double sideA = newSideC - newSideB;
+	return sqrt(sideA);
+}
+
+double solveForB(double sideA, double sideC) {
+	double newSideA = sideA * sideA;
+	double newSideC = sideC * sideC;
+	double sideB = newSideC - newSideA;
+	return sqrt(sideB);
+}
+
+double rightTriangle(char whichSide) {
+	double sideOne;
+	double sideTwo;
+	double result;
+
+	if (whichSide == 'A') {
+		cout << "Enter length for side B >> ";
+		cin >> sideOne;
+		cout << "Enter length for side C >> ";
+		cin >> sideTwo;
+		result = solveForA(sideOne, sideTwo);
+		setPerimeter(result, sideOne, sideTwo);
+		setRightArea(result, sideOne);
+		return result;
+	}
+	else if (whichSide == 'B') {
+		cout << "Enter length for side A >> ";
+		cin >> sideOne;
+		cout << "Enter length for side C >> ";
+		cin >> sideTwo;
+		result = solveForB(sideOne, sideTwo);
+		setPerimeter(sideOne, result, sideTwo);
+		setRightArea(sideOne, result);
+		return result;
+	}
+	else if (whichSide == 'C') {
+		cout << "Enter length for side A >> ";
+		cin >> sideOne;
+		cout << "Enter length for side B >> ";
+		cin >> sideTwo;
+		result = hypot(sideOne, sideTwo);
+		setPerimeter(sideOne, sideTwo, result);
+		setRightArea(sideOne, sideTwo);
+		return result;
+	}
+}
 
 int main() {
 	int triangleType;
@@ -11,7 +80,6 @@ int main() {
 	string stop;
 	char whichSide;
 
-	
 		cout << "Hello user. What kind of triangle are you trying to solve for?" << endl;
 		cout << "1 - Right Triangle" << endl;
 		cout << "2 - Isosceles Triangle" << endl;
@@ -23,7 +91,9 @@ int main() {
 			cout << "Which side are you solving for? (A, B or C) >> ";
 			cin >> whichSide;
 			output = rightTriangle(whichSide);
-			cout << output;
+			cout << "Your missing side has a length of: " << output << endl;
+			cout << "Perimeter is: " << getPerimeter() << endl;
+			cout << "Area is: " << getArea() << endl;
 		}
 	
 	system("pause");
